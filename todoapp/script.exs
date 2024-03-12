e64b0654b0c153753a4720d5bfb10d17df57b6aa
Luiz Felipe Escandiuzzi <lfescandiuzzi@icloud.com>
2024-03-12 15:40:54 -0300

    Add CsvImporter

--- todoapp/script.exs
+++ todoapp/script.exs
@@ -1,15 +1,28 @@
-date = {2023, 3, 12}
-entry = %{date: date, title: "Code"}
+# date = {2023, 3, 12}
+# entry = %{date: date, title: "Code"}
 
-todo_list = TodoList.new
-todo_list = TodoList.add_entry(todo_list, entry)
+# todo_list = TodoList.new
+# todo_list = TodoList.add_entry(todo_list, entry)
 
-date = {2023, 3, 12}
-entry = %{date: date, title: "Gym"}
-todo_list = TodoList.add_entry(todo_list, entry)
+# date = {2023, 3, 12}
+# entry = %{date: date, title: "Gym"}
+# todo_list = TodoList.add_entry(todo_list, entry)
 
-date2 = {2023, 3, 13}
-entry = %{date: date2, title: "Code"}
-todo_list = TodoList.add_entry(todo_list, entry)
+# date2 = {2023, 3, 13}
+# entry = %{date: date2, title: "Code"}
+# todo_list = TodoList.add_entry(todo_list, entry)
 
-IO.inspect(TodoList.entries(todo_list, date))
+# IO.puts("Initial")
+# IO.inspect(TodoList.entries(todo_list, date))
+
+# IO.puts("After update")
+# todo_list = TodoList.update_entry(todo_list, 2, &Map.put(&1, :title, "Study"))
+# IO.inspect(TodoList.entries(todo_list, date))
+
+# todo_list = TodoList.delete_entry(todo_list, 2);
+
+# IO.puts("After delete")
+# IO.inspect(TodoList.entries(todo_list, date))
+
+
+IO.inspect(CsvImporter.import!("./resources/todos.csv"))
--- todoapp/todolist.ex
+++ todoapp/todolist.ex
@@ -1,7 +1,13 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: Map.new()
 
-  def new, do: %TodoList{}
+  def new(entries \\ []) do
+    entries
+    # fn entry, todo_list_acc -> add_entry(todo_list_acc, entry) end)
+    |> Enum.reduce(%TodoList{}, &add_entry(&2, &1))
+  end
+
+  # def new, do: %TodoList{}
 
   def add_entry(%TodoList{entries: entries, auto_id: auto_id} = todo_list, entry) do
     entry = Map.put(entry, :id, auto_id)
@@ -17,6 +23,62 @@ defmodule TodoList do
   def entries(%TodoList{entries: entries}, date) do
     entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
-    |> Enum.map(fn ({_, entry}) -> entry end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(%TodoList{entries: entries} = todo_list, entry_id, updated_func) do
+    case entries[entry_id] do
+      nil ->
+        todo_list
+
+      old_entry ->
+        old_entry_id = old_entry.id
+
+        new_entry = %{id: ^old_entry_id} = updated_func.(old_entry)
+        new_entries = Map.put(entries, entry_id, new_entry)
+
+        %TodoList{
+          todo_list
+          | entries: new_entries
+        }
+    end
+  end
+
+  def delete_entry(%TodoList{entries: entries, auto_id: auto_id} = todo_list, delete_id)
+      when is_number(delete_id) do
+    new_entries = Map.delete(entries, delete_id)
+
+    %TodoList{
+      todo_list
+      | entries: new_entries,
+        auto_id: auto_id - 1
+    }
+  end
+end
+
+defmodule CsvImporter do
+  def import!(path) do
+    path
+    |> File.stream!()
+    |> Enum.map(&parse_line(&1))
+    |> TodoList.new
+  end
+
+  defp parse_line(line) do
+    line = String.replace(line, "\n", "")
+
+    [date_string, title] = String.split(line, ",")
+    [year_string, month_string, day_string] = String.split(date_string, "/")
+
+    %{
+      date:
+        {String.to_integer(year_string), String.to_integer(month_string),
+         String.to_integer(day_string)},
+      title: title
+    }
   end
 end
